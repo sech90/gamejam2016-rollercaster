e387a31138076927493e10de6fd76d93bf5d8d43
Christian <ccardin.sech@gmail.com>
2016-01-30 04:04:05 +0200

    Added triangle counter

---
+++ RollerCaster/Assets/Scripts/Utils/TriangleCounter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriangleCounter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MeshFilter filter = GetComponent<MeshFilter>();
+		int[] tris = filter.mesh.triangles;
+		Vector3[] vertices = filter.mesh.vertices;
+
+		Debug.Log(name+ " has "+tris.Length/3+" triangles and "+vertices.Length+" vertices");
+	}
+}
