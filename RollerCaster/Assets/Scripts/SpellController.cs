b0c072509ec6cd0c5290563490be93ee478c24f9
Anh <quanganh.tran94@gmail.com>
2016-01-30 04:01:17 +0200

    Spell and SpellController

---
+++ RollerCaster/Assets/Scripts/SpellController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellController : MonoBehaviour {
+	public Spell spell;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// On collision with another spell
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log (name + " fired");
+		if (!other.gameObject)
+			return;
+		SpellController sc = other.gameObject.GetComponent<SpellController> ();
+		if (!sc)
+			return;
+		Debug.Log ("pitching " + spell + " against " + sc.spell);
+		if (!spell.Resolve (sc.spell)) {
+			Destroy (gameObject);
+		}
+	}
+
+	// Called when the spell game object is destroyed
+	void OnDestroy() {
+		Debug.Log ("Spell destroyed!");
+	}
+		
+}
